5f9f59ba2c9cbe51a7ba7c968c22be517106abde
Louis Schell <louis.schell1@gmx.de>
2026-02-18 14:52:03 +0100

    fix g4 clock configuration

--- shared/stm32g473_clocks.rs
+++ shared/stm32g473_clocks.rs
@@ -1,23 +1,36 @@
 pub fn clocks_config() -> embassy_stm32::Config {
     use embassy_stm32::rcc::{
-        Hse, HseMode, Pll, PllMul, PllPDiv, PllPreDiv, PllQDiv, PllRDiv, PllSource,
+        AHBPrescaler, APBPrescaler, Hse, HseMode, Pll, PllMul, PllPreDiv, PllQDiv, PllRDiv,
+        PllSource, Sysclk,
     };
     use embassy_stm32::time::Hertz;
 
     let mut config = embassy_stm32::Config::default();
 
+    // clck running at 160 MHz (max for G473), pulsed
+    // by 16 MHz external crystal
     config.rcc.hse = Some(Hse {
         freq: Hertz(16_000_000),
         mode: HseMode::Oscillator,
     });
+
     config.rcc.pll = Some(Pll {
         source: PllSource::HSE,
         prediv: PllPreDiv::DIV1,
         mul: PllMul::MUL20,
-        divp: Some(PllPDiv::DIV2),
-        divq: Some(PllQDiv::DIV2),
         divr: Some(PllRDiv::DIV2),
+        divq: Some(PllQDiv::DIV2),
+        divp: None,
     });
+
+    config.rcc.sys = Sysclk::PLL1_R;
+
+    config.rcc.ahb_pre = AHBPrescaler::DIV1;
+    config.rcc.apb1_pre = APBPrescaler::DIV1;
+    config.rcc.apb2_pre = APBPrescaler::DIV1;
+
+    // recommended when SYSCLK > 150 MHz on G4
+    config.rcc.boost = true;
 
     config
 }
