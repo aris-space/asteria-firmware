7e0858d924c85812d29778771e01360f1dd652db
Cyrill Bucher <indeximal@gmail.com>
2026-04-25 20:10:42 +0200

    add a responsive Loop implementation

--- crates/can-utils/src/broadcast.rs
+++ crates/can-utils/src/broadcast.rs
@@ -1,8 +1,11 @@
 use crate::rxtx::TypedCanTransmit;
+use can_hal::CanEncode;
 use embassy_executor::{SpawnError, Spawner};
 use embassy_stm32::can::CanTx;
 use embassy_sync::blocking_mutex::raw::{RawMutex, ThreadModeRawMutex};
 use embassy_sync::mutex::Mutex;
+use embassy_time::Duration;
+use embassy_time::Instant;
 
 pub use broadcast_derive::Broadcast;
 
@@ -57,3 +60,64 @@ pub trait BroadcastLoop<M>: 'static {
 
 /// A type which can't be constructed and as such a function which returns this cannot return.
 pub enum NeverReturns {}
+
+/// A [`BroadcastLoop`] implementation that sends new values as they arrive,
+/// while respecting the minimum and maximum frequencies.
+///
+/// The way this works is by waiting for new data or a timeout based on the minimum frequency.
+/// If the duration since the last sent message is long enough to respect the maximum frequency,
+/// it is sent, otherwise ignored.
+pub struct ResponsiveLoop;
+
+impl<M> BroadcastLoop<M> for ResponsiveLoop
+where
+    M: CanEncode + embedded_utils::fmt::Format + Send,
+    <M as CanEncode>::Error: embedded_utils::fmt::Format,
+{
+    async fn broadcast_loop<
+        T: Send + Sync + 'static + Clone,
+        MTX: embassy_sync::blocking_mutex::raw::RawMutex + Sync,
+        const N: usize,
+    >(
+        mut watch: embassy_sync::watch::Receiver<'static, MTX, T, N>,
+        mut filter_map: impl FnMut(T) -> Option<M> + Send + 'static,
+        transmit: &'static Mutex<MTX, impl TypedCanTransmit>,
+        min_freq_hz: f32,
+        max_freq_hz: f32,
+    ) -> NeverReturns {
+        const TX_TIMEOUT: Duration = Duration::from_millis(100);
+
+        let mut last_sent = Instant::now();
+        let throttle_period = Duration::from_millis((1000. / max_freq_hz) as u64);
+        let resend_period = Duration::from_millis((1000. / min_freq_hz) as u64);
+
+        loop {
+            let data = match embassy_time::with_timeout(resend_period, watch.changed()).await {
+                Ok(t) => t,
+                Err(_) => watch.get().await,
+            };
+            let Some(msg) = filter_map(data) else {
+                embedded_utils::trace!("Skipping new data of type {}", core::any::type_name::<T>());
+                continue;
+            };
+
+            let now = Instant::now();
+            if now - last_sent >= throttle_period {
+                let mut tx = transmit.lock().await;
+                embedded_utils::trace!("Sending data: {:?}", msg);
+                match embassy_time::with_timeout(TX_TIMEOUT, tx.transmit(msg)).await {
+                    Ok(Ok(())) => {
+                        last_sent = now;
+                    }
+                    Ok(Err(err)) => embedded_utils::error!("CAN TX error: {:?}", err),
+                    Err(_) => embedded_utils::error!(
+                        "CAN TX timed out after {} ms",
+                        TX_TIMEOUT.as_millis()
+                    ),
+                }
+            } else {
+                embedded_utils::trace!("Discarding data: {:?}", msg);
+            }
+        }
+    }
+}
