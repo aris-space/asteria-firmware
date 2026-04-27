3f25a63e1359c82e67633435257eca6b274198a4
Thierry Marti <thierry.robin.marti@outlook.de>
2026-04-27 17:18:18 +0200

    remove Camera implementation

--- fw-power-board/app/src/can.rs
+++ fw-power-board/app/src/can.rs
@@ -1,11 +1,9 @@
-use crate::board::{CAMERA_SETTINGS_CHANGED, LIVESTREAM_CAMERA, RECORDING_CAMERA};
 use crate::can_impl::CanReceiver;
 use crate::can_impl::{CanError, CanTransmitter};
 use crate::reset_now;
 use crate::sensor_readout::{RAIL_5V_LAST, RAIL_24V_LAST};
 use crate::unix_time::init_utc_clock;
 use core::sync::atomic::Ordering;
-use embassy_futures::select::select;
 use embassy_stm32::can::{CanRx, CanTx};
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_sync::mutex::Mutex;
@@ -13,9 +11,7 @@ use embassy_sync::once_lock::OnceLock;
 use embassy_time::{Duration, Instant, Ticker, Timer, with_timeout};
 use embedded_utils::ERROR_COUNT;
 use embedded_utils::fmt::{error, info, trace};
-use hermes_can::messages::board_status::{
-    CameraPowerStatus, PowerBoardStatus, StatusCommonMessage,
-};
+use hermes_can::messages::board_status::{PowerBoardStatus, StatusCommonMessage};
 use hermes_can::messages::{BoardId, Message};
 
 const CAN_SEND_TIMEOUT: Duration = Duration::from_millis(100);
@@ -42,8 +38,6 @@ pub async fn spawn_can_tasks(
     spawner.spawn(can_24v_task(can_tx).expect("Failed to spawn CAN 24V task"));
 
     spawner.spawn(board_status(can_tx).expect("Failed to spawn board status task"));
-
-    spawner.spawn(camera_status(can_tx).expect("Failed to spawn camera status task"));
 
     spawner.spawn(build_information(can_tx).expect("Failed to spawn build information task"));
 }
@@ -148,41 +142,6 @@ async fn can_5v_task(can_tx: &'static Mutex<ThreadModeRawMutex, CanTx<'static>>)
 }
 
 #[embassy_executor::task]
-async fn camera_status(can_tx: &'static Mutex<ThreadModeRawMutex, CanTx<'static>>) {
-    // pin handles
-    let recording_camera = RECORDING_CAMERA.get().await;
-    let live_camera = LIVESTREAM_CAMERA.get().await;
-
-    // sent at 5 hz
-    let mut ticker = Ticker::every(Duration::from_millis(200));
-
-    loop {
-        let rec_enabled = recording_camera.lock().await.is_set_high();
-        let live_enabled = live_camera.lock().await.is_set_high();
-
-        let camera_status = CameraPowerStatus {
-            recording_pwr_enabled: rec_enabled,
-            live_pwr_enabled: live_enabled,
-        };
-
-        // this scope is necessary to ensure the lock is released – do not remove it!
-        {
-            let mut tx = can_tx.lock().await;
-            match with_timeout(CAN_SEND_TIMEOUT, tx.transmit(camera_status.clone())).await {
-                Ok(Ok(())) => trace!("Sent CameraPowerStatus: {:?}", camera_status),
-                Ok(Err(err)) => error!("Error sending CameraPowerStatus: {:?}", err),
-                Err(_) => error!(
-                    "Timeout sending CameraPowerStatus after {} ms",
-                    CAN_SEND_TIMEOUT.as_millis()
-                ),
-            }
-        }
-
-        select(CAMERA_SETTINGS_CHANGED.wait(), ticker.next()).await;
-    }
-}
-
-#[embassy_executor::task]
 async fn board_status(can_tx: &'static Mutex<ThreadModeRawMutex, CanTx<'static>>) {
     // send once per second
     let mut ticker = Ticker::every(Duration::from_millis(1000));
@@ -240,9 +199,6 @@ async fn build_information(can_tx: &'static Mutex<ThreadModeRawMutex, CanTx<'sta
 
 #[embassy_executor::task]
 async fn can_rx_task(mut rx: CanRx<'static>) -> ! {
-    let rec_pin = RECORDING_CAMERA.get().await;
-    let live_pin = LIVESTREAM_CAMERA.get().await;
-
     loop {
         match rx.recv().await {
             Ok((msg, ts)) => {
@@ -261,17 +217,6 @@ async fn can_rx_task(mut rx: CanRx<'static>) -> ! {
                     }
                     Message::UTCTimeUpdate(time) => {
                         init_utc_clock(time.timestamp_micros, ts);
-                    }
-                    Message::CameraPowerControl(cmd) => {
-                        {
-                            let mut rec_camera = rec_pin.lock().await;
-                            rec_camera.set_level(cmd.recording_pwr_enabled.into());
-                        }
-                        {
-                            let mut live_camera = live_pin.lock().await;
-                            live_camera.set_level(cmd.live_pwr_enabled.into());
-                        }
-                        CAMERA_SETTINGS_CHANGED.signal(());
                     }
                     #[allow(unreachable_patterns)]
                     _ => {
