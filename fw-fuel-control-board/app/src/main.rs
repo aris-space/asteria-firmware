d01fad47a815572239d42e3d593e40f2292ad7e2
Carl <184755785+CFCBerlin@users.noreply.github.com>
2026-05-19 17:38:10 +0200

    removed debugging left oovers

--- fw-fuel-control-board/app/src/main.rs
+++ fw-fuel-control-board/app/src/main.rs
@@ -85,9 +85,6 @@ async fn main(spawner: Spawner) -> ! {
     set_adc_configs(&mut config);
     let p = embassy_stm32::init(config);
 
-    //todo() fixn this delay after debuging
-    Timer::after_millis(1000).await;
-
     // LEDs
     let _green = Output::new(p.PC15, Level::High, Speed::Low);
     let _yellow = Output::new(p.PC14, Level::High, Speed::Low);
@@ -135,12 +132,12 @@ async fn main(spawner: Spawner) -> ! {
     );
 
     // Can Bus
-    /*let can = setup_can(p.FDCAN1, p.PB8, p.PB9, Irqs, ReceivedMessage::SUPPORTED_IDS);
+    let can = setup_can(p.FDCAN1, p.PB8, p.PB9, Irqs, ReceivedMessage::SUPPORTED_IDS);
     let (tx, rx, _) = can.split();
     let tx = make_multiplexable(tx);
     STATE
         .start_broadcasting(spawner, tx)
-        .expect("failed to start CAN broadcasting");*/
+        .expect("failed to start CAN broadcasting");
 
     spawner.spawn(
         dpr::pid_controller(
@@ -153,7 +150,7 @@ async fn main(spawner: Spawner) -> ! {
         .expect("failed to prepare pid_controller spawn token"),
     );
 
-    /*spawner.spawn(
+    spawner.spawn(
         valve_task(pressurization_vent_valve, fuel_vent_valve)
             .expect("failed to prepare valve_task spawn token"),
     );
@@ -168,10 +165,10 @@ async fn main(spawner: Spawner) -> ! {
     );
 
     spawner.spawn(buzzer_task(buzzer_pwm).expect("failed to prepare buzzer_task spawn token"));
-    */
+
     spawner.spawn(
         fuel_pressure_acquisition(pressure_handles).expect("failed to prepare pressure task"),
-    );/*
+    );
 
     // TODO: Replace PA0/PA1/PA2 with the correct solenoid detection pins
     spawner.spawn(
@@ -181,9 +178,7 @@ async fn main(spawner: Spawner) -> ! {
     spawner.spawn(
         solenoid_current_task(solenoid_current_i2c)
             .expect("failed to prepare solenoid current task"),
-    );*/
-
-    STATE.dpr_control_loop.sender().send(DPRValve::Enabled {setpoint: 50.0});
+    );
 
     #[allow(unreachable_code)]
     loop {
