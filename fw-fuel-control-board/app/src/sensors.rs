5c62d6f70fcde21643dfb4729be6eb1f2d2cc8f1
Carl <184755785+CFCBerlin@users.noreply.github.com>
2026-05-21 22:00:43 +0200

    updated the pressure sensor ranges

--- fw-fuel-control-board/app/src/sensors.rs
+++ fw-fuel-control-board/app/src/sensors.rs
@@ -4,8 +4,8 @@ pub mod solenoid_current;
 pub(crate) const ACQ_PRESSURE_FREQ_HZ: f32 = 600.0; // max stable possible ~ tbd Hz
 pub(crate) const ADC_CALIBRATION_SAMPLES: u64 = 50;
 
-pub(crate) const PRESSURIZATION_PRESSURE_RANGE: [f32; 2] = [0.0, 100.0];
-pub(crate) const FUEL_TANK_PRESSURE_1_RANGE: [f32; 2] = [0.0, 100.0];
-pub(crate) const FUEL_TANK_PRESSURE_2_RANGE: [f32; 2] = [0.0, 100.0];
+pub(crate) const PRESSURIZATION_PRESSURE_RANGE: [f32; 2] = [-1.0, 99.0];
+pub(crate) const FUEL_TANK_PRESSURE_1_RANGE: [f32; 2] = [-1.0, 99.0];
+pub(crate) const FUEL_TANK_PRESSURE_2_RANGE: [f32; 2] = [-1.0, 99.0];
 
 pub(crate) const CAN_BOARD_STATUS_FREQ_HZ: f32 = 1.0;
