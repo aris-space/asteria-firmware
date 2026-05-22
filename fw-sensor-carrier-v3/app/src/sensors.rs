a746eb73c4a9f5001674a4c9735e06e34b4cb952
Louis Schell <louis.schell1@gmx.de>
2026-05-22 16:13:07 +0200

    Derive sensor family count from the id list in the macro

--- fw-sensor-carrier-v3/app/src/sensors.rs
+++ fw-sensor-carrier-v3/app/src/sensors.rs
@@ -2,7 +2,7 @@ macro_rules! define_sensor_family {
     (
         $(#[$meta:meta])*
         $vis:vis struct $name:ident;
-        count: $count_name:ident = $count:expr;
+        count: $count_name:ident;
         ids: [$($id_name:ident = $index:expr),+ $(,)?];
     ) => {
         $(#[$meta])*
@@ -15,38 +15,38 @@ macro_rules! define_sensor_family {
             }
         }
 
-        $vis const $count_name: usize = $count;
+        $vis const $count_name: usize = [$(stringify!($id_name)),+].len();
         $( $vis const $id_name: $name = $name($index); )+
     };
 }
 
 define_sensor_family! {
     pub struct ImuId;
-    count: IMU_COUNT = 2;
+    count: IMU_COUNT;
     ids: [IMU_0 = 0, IMU_1 = 1];
 }
 
 define_sensor_family! {
     pub struct BarometerId;
-    count: BAROMETER_COUNT = 2;
+    count: BAROMETER_COUNT;
     ids: [BAROMETER_0 = 0, BAROMETER_1 = 1];
 }
 
 define_sensor_family! {
     pub struct MagnetometerId;
-    count: MAGNETOMETER_COUNT = 2;
+    count: MAGNETOMETER_COUNT;
     ids: [MAGNETOMETER_0 = 0, MAGNETOMETER_1 = 1];
 }
 
 define_sensor_family! {
     pub struct GnssId;
-    count: GNSS_COUNT = 2;
+    count: GNSS_COUNT;
     ids: [GNSS_0 = 0, GNSS_1 = 1];
 }
 
 define_sensor_family! {
     pub struct DhtId;
-    count: DHT_COUNT = 2;
+    count: DHT_COUNT;
     ids: [DHT_0 = 0, DHT_1 = 1];
 }
 
