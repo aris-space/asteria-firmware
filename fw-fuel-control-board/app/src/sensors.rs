e80445358177271972940c57a6214376fb4578e4
Carl <184755785+CFCBerlin@users.noreply.github.com>
2026-05-22 11:25:57 +0200

    prz range

--- fw-fuel-control-board/app/src/sensors.rs
+++ fw-fuel-control-board/app/src/sensors.rs
@@ -4,7 +4,7 @@ pub mod solenoid_current;
 pub(crate) const ACQ_PRESSURE_FREQ_HZ: f32 = 600.0; // max stable possible ~ tbd Hz
 pub(crate) const ADC_CALIBRATION_SAMPLES: u64 = 50;
 
-pub(crate) const PRESSURIZATION_PRESSURE_RANGE: [f32; 2] = [-1.0, 99.0];
+pub(crate) const PRESSURIZATION_PRESSURE_RANGE: [f32; 2] = [-1.0, 399.0];
 pub(crate) const FUEL_TANK_PRESSURE_1_RANGE: [f32; 2] = [-1.0, 99.0];
 pub(crate) const FUEL_TANK_PRESSURE_2_RANGE: [f32; 2] = [-1.0, 99.0];
 
