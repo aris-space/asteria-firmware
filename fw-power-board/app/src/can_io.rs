0feb1a2840ab5d60bb9232af4b44721aba9550db
Thierry Marti <thierry.robin.marti@outlook.de>
2026-05-23 11:47:17 +0200

    finish new CAN implementation

--- fw-power-board/app/src/can_io.rs
+++ fw-power-board/app/src/can_io.rs
@@ -1,6 +1,6 @@
 use data_core::can::{hal::CanDecode, sparse_decodable_can_message};
 
-sparse_decodable_can_message!{
+sparse_decodable_can_message! {
   enum ReceivedMessage {
     ResetAll(dp_system_management::Message::ResetAll),
     ResetSpecific(dp_system_management::Message::ResetSpecific),
@@ -9,8 +9,8 @@ sparse_decodable_can_message!{
 }
 
 const __ASSERT_LEN_OK: () = {
-  const FILTER_COUNT: usize = 28;
-  if ReceivedMessage::SUPPORTED_IDS.len() >= FILTER_COUNT - 1 {
-      core::panic!("Too many receiving can ids");
-  }
+    const FILTER_COUNT: usize = 28;
+    if ReceivedMessage::SUPPORTED_IDS.len() >= FILTER_COUNT - 1 {
+        core::panic!("Too many receiving can ids");
+    }
 };
