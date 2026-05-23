5c5e372f18cac1d8cb1cd6331eb0dd30da3b563e
Louis Schell <louis.schell1@gmx.de>
2026-05-23 23:34:25 +0200

    Label sensor-id constants with interned defmt output

--- fw-sensor-carrier-v3/app/src/sensors.rs
+++ fw-sensor-carrier-v3/app/src/sensors.rs
@@ -3,15 +3,38 @@ macro_rules! define_sensor_family {
         $(#[$meta:meta])*
         $vis:vis struct $name:ident;
         count: $count_name:ident;
-        ids: [$($id_name:ident = $index:expr),+ $(,)?];
+        ids: [$($id_name:ident = $index:literal => $label:literal),+ $(,)?];
     ) => {
         $(#[$meta])*
-        #[derive(Debug, Clone, Copy, PartialEq, Eq, defmt::Format)]
+        #[derive(Clone, Copy, PartialEq, Eq)]
         $vis struct $name(u8);
 
         impl $name {
             pub const fn index(self) -> usize {
                 self.0 as usize
+            }
+
+            pub const fn name(self) -> &'static str {
+                match self.0 {
+                    $($index => $label,)+
+                    _ => "<unknown>",
+                }
+            }
+        }
+
+        impl core::fmt::Debug for $name {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                f.write_str(self.name())
+            }
+        }
+
+        impl defmt::Format for $name {
+            fn format(&self, fmt: defmt::Formatter) {
+                let label = match self.0 {
+                    $($index => defmt::intern!($label),)+
+                    _ => defmt::intern!("<unknown>"),
+                };
+                defmt::write!(fmt, "{=istr}", label)
             }
         }
 
@@ -23,31 +46,31 @@ macro_rules! define_sensor_family {
 define_sensor_family! {
     pub struct ImuId;
     count: IMU_COUNT;
-    ids: [IMU_0 = 0, IMU_1 = 1];
+    ids: [IMU_0 = 0 => "IMU 0",IMU_1 = 1 => "IMU 1",];
 }
 
 define_sensor_family! {
     pub struct BarometerId;
     count: BAROMETER_COUNT;
-    ids: [BARO_BUS_1 = 0, BARO_BUS_2 = 1];
+    ids: [BARO_BUS_1 = 0 => "Baro Bus 1", BARO_BUS_2 = 1 => "Baro Bus 2"];
 }
 
 define_sensor_family! {
     pub struct MagnetometerId;
     count: MAGNETOMETER_COUNT;
-    ids: [MAG_BUS_1 = 0, MAG_BUS_2 = 1];
+    ids: [MAG_BUS_1 = 0 => "Mag Bus 1", MAG_BUS_2 = 1 => "Mag Bus 2"];
 }
 
 define_sensor_family! {
     pub struct GnssId;
     count: GNSS_COUNT;
-    ids: [GNSS_0 = 0, GNSS_1 = 1];
+    ids: [GNSS_0 = 0 => "GNSS 0", GNSS_1 = 1 => "GNSS 1"];
 }
 
 define_sensor_family! {
     pub struct DhtId;
     count: DHT_COUNT;
-    ids: [DHT_BUS_1 = 0, DHT_BUS_2 = 1];
+    ids: [DHT_BUS_1 = 0 => "DHT Bus 1", DHT_BUS_2 = 1 => "DHT Bus 2"];
 }
 
 // --- Health tracking (CAN status frame) -------------------------------------
